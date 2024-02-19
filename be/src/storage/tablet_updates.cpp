7500996ec1d9833aa962ad3dbd259a8333c78e1e
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2024-02-19 17:09:06 +0800

    [Stella][Enhancement] Print disk path for compaction
    Link: https://code.alibaba-inc.com/soe/starrocks/codereview/15592755
    
    * [EMR][Enhancement] Print disk path for compaction

--- be/src/storage/tablet_updates.cpp
+++ be/src/storage/tablet_updates.cpp
@@ -2263,7 +2263,7 @@ Status TabletUpdates::_commit_compaction(std::unique_ptr<CompactionInfo>* pinfo,
     }
     LOG(INFO) << "commit compaction tablet:" << _tablet.tablet_id()
               << " version:" << edit_version_info_ptr->version.to_string();
-    VLOG(1) << "commit compaction tablet:" << _tablet.tablet_id()
+    VLOG(1) << "commit compaction tablet:" << _tablet.tablet_id() << ", disk:" << _tablet.data_dir()->path()
             << " version:" << edit_version_info_ptr->version.to_string() << " rowset:" << rowsetid
             << " #seg:" << rowset->num_segments() << " #row:" << rowset->num_rows()
             << " size:" << PrettyPrinter::print(rowset->data_disk_size(), TUnit::BYTES)
@@ -3017,7 +3017,7 @@ Status TabletUpdates::compaction(MemTracker* mem_tracker) {
         return Status::OK();
     }
     std::sort(info->inputs.begin(), info->inputs.end());
-    VLOG(1) << "update compaction start tablet:" << _tablet.tablet_id()
+    VLOG(1) << "update compaction start tablet:" << _tablet.tablet_id() << ", disk:" << _tablet.data_dir()->path()
             << " version:" << info->start_version.to_string() << " score:" << total_score
             << " pick:" << info->inputs.size() << "/valid:" << total_valid_rowsets << "/all:" << rowsets.size() << " "
             << int_list_to_string(info->inputs) << " #pick_segments:" << total_segments
