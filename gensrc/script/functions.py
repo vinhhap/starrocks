640cb608431eae5e05646c7554e243a4c29e39b3
wuxueyang.wxy <wuxueyang.wxy@alibaba-inc.com>
2024-05-06 11:33:49 +0800

    [Stella][BP-Enhancement] Support tokenize function (#45119)
    Signed-off-by: leorishdu <18771113323@163.com>

--- gensrc/script/functions.py
+++ gensrc/script/functions.py
@@ -1323,6 +1323,6 @@ vectorized_functions = [
     [170500, 'row', True, False, 'ANY_STRUCT', ['ANY_ELEMENT', "..."], 'StructFunctions::new_struct'],
     [170501, 'named_struct', True, False, 'ANY_STRUCT', ['ANY_ELEMENT', "..."], 'StructFunctions::named_struct'],
 
-    # user function
-    [180000, 'is_role_in_session', True, False, 'BOOLEAN', ['VARCHAR'], 'nullptr']
+    # gin functions
+    [190000, 'tokenize', True, False, 'ARRAY_VARCHAR', ['VARCHAR', 'VARCHAR'], 'GinFunctions::tokenize', 'GinFunctions::tokenize_prepare', 'GinFunctions::tokenize_close']
 ]
