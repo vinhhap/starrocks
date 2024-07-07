5051467dfb0c3c2e165845560f134ccf5a2607a7
drake_wang <wxl250059@alibaba-inc.com>
2024-07-07 22:07:46 +0800

    [EMR][Feature] Add REST API for starmgr diagnostic

--- fe/fe-core/src/main/java/com/starrocks/http/HttpServer.java
+++ fe/fe-core/src/main/java/com/starrocks/http/HttpServer.java
@@ -85,6 +85,7 @@ import com.starrocks.http.rest.ShowDataAction;
 import com.starrocks.http.rest.ShowMetaInfoAction;
 import com.starrocks.http.rest.ShowProcAction;
 import com.starrocks.http.rest.ShowRuntimeInfoAction;
+import com.starrocks.http.rest.StarManagerHttpServiceAction;
 import com.starrocks.http.rest.StopFeAction;
 import com.starrocks.http.rest.StorageTypeCheckAction;
 import com.starrocks.http.rest.SyncCloudTableMetaAction;
@@ -201,6 +202,7 @@ public class HttpServer {
         // for stop FE
         StopFeAction.registerAction(controller);
         ExecuteSqlAction.registerAction(controller);
+        StarManagerHttpServiceAction.registerAction(controller);
 
         // meta service action
         File imageDir = MetaHelper.getLeaderImageDir();
