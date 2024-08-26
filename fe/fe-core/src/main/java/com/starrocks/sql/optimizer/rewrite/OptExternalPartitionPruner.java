a201aa79fa1da6f7255bf72904a25a6f0a2ddf48
wangriyu.wry <wangriyu.wry@alibaba-inc.com>
2024-08-26 16:51:36 +0800

    [Stella][BugFix] Remove paimon partition prune and statistics

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/OptExternalPartitionPruner.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/OptExternalPartitionPruner.java
@@ -34,10 +34,8 @@ import com.starrocks.catalog.Table;
 import com.starrocks.common.AnalysisException;
 import com.starrocks.common.Pair;
 import com.starrocks.common.util.DebugUtil;
-import com.starrocks.connector.RemoteFileInfo;
 import com.starrocks.connector.elasticsearch.EsShardPartitions;
 import com.starrocks.connector.elasticsearch.EsTablePartitions;
-import com.starrocks.connector.paimon.PaimonRemoteFileDesc;
 import com.starrocks.planner.PartitionColumnFilter;
 import com.starrocks.planner.PartitionPruner;
 import com.starrocks.planner.RangePartitionPruner;
@@ -60,7 +58,6 @@ import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rule.transformation.ListPartitionPruner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.paimon.table.source.Split;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -75,7 +72,6 @@ import java.util.stream.Collectors;
 
 import static com.starrocks.connector.PartitionUtil.createPartitionKey;
 import static com.starrocks.connector.PartitionUtil.toPartitionValues;
-import static com.starrocks.connector.paimon.PaimonMetadata.getRowCount;
 
 public class OptExternalPartitionPruner {
     private static final Logger LOG = LogManager.getLogger(OptExternalPartitionPruner.class);
@@ -442,28 +438,7 @@ public class OptExternalPartitionPruner {
             scanOperatorPredicates.getIdToPartitionKey().putAll(partitionKeyMap);
             scanOperatorPredicates.setSelectedPartitionIds(partitionKeyMap.keySet());
         } else if (table instanceof PaimonTable) {
-            PaimonTable paimonTable = (PaimonTable) table;
-            List<String> fieldNames = operator.getColRefToColumnMetaMap().keySet().stream()
-                    .map(ColumnRefOperator::getName)
-                    .collect(Collectors.toList());
-            List<RemoteFileInfo> fileInfos = GlobalStateMgr.getCurrentState().getMetadataMgr().getRemoteFileInfos(
-                    paimonTable.getCatalogName(), table, null, -1, operator.getPredicate(), fieldNames, -1);
-            if (fileInfos.isEmpty()) {
-                return;
-            }
-
-            PaimonRemoteFileDesc remoteFileDesc = (PaimonRemoteFileDesc) fileInfos.get(0).getFiles().get(0);
-            if (remoteFileDesc == null) {
-                return;
-            }
-            List<Split> splits = remoteFileDesc.getPaimonSplitsInfo().getPaimonSplits();
-            if (splits.isEmpty()) {
-                return;
-            }
-            long rowCount = getRowCount(splits);
-            if (rowCount > 0) {
-                scanOperatorPredicates.getSelectedPartitionIds().add(1L);
-            }
+            scanOperatorPredicates.getSelectedPartitionIds().add(1L);
         }
     }
 
