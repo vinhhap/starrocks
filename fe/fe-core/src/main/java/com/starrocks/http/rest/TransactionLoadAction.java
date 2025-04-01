f4653f840da02ad51f57984d914684a4f70ac066
wuxueyang.wxy <wuxueyang.wxy@alibaba-inc.com>
2025-04-01 14:34:40 +0800

    [Stella][Enhancement] Support force use ip in Transaction Load.
    Link: https://code.alibaba-inc.com/soe/stella/codereview/21065884

--- fe/fe-core/src/main/java/com/starrocks/http/rest/TransactionLoadAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/TransactionLoadAction.java
@@ -38,6 +38,7 @@ import com.codahale.metrics.Histogram;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.starrocks.catalog.Database;
+import com.starrocks.common.Config;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.LabelAlreadyUsedException;
 import com.starrocks.common.StarRocksHttpException;
@@ -70,9 +71,12 @@ import com.starrocks.warehouse.Warehouse;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.InetAddressValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -234,6 +238,20 @@ public class TransactionLoadAction extends RestBaseAction {
             }
 
             redirectAddress = new TNetworkAddress(node.getHost(), node.getHttpPort());
+        }
+
+        if (Config.stream_load_force_use_ip) {
+            InetAddressValidator validator = InetAddressValidator.getInstance();
+            String redirectHost = redirectAddress.getHostname();
+            if (!validator.isValidInet4Address(redirectHost) && !validator.isValidInet6Address(redirectHost)) {
+                try {
+                    InetAddress ipAddress = InetAddress.getByName(redirectHost);
+                    redirectHost = ipAddress.getHostAddress();
+                } catch (UnknownHostException ex) {
+                    LOG.warn("get redirect host for leader {} failed!", redirectHost);
+                }
+            }
+            redirectAddress = new TNetworkAddress(redirectHost, redirectAddress.getPort());
         }
 
         LOG.info("Redirect transaction action to destination={}, db: {}, table: {}, op: {}, label: {}, warehouse: {}",
