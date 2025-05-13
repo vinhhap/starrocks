ac81891299d563bc5ba2d01b34bb0dc15ea95562
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2025-05-13 19:49:05 +0800

    [Stella][BugFix] Fix NPE when show partitions of a non partition paimon table

--- fe/fe-core/src/main/java/com/starrocks/common/proc/PaimonTablePartitionsProcDir.java
+++ fe/fe-core/src/main/java/com/starrocks/common/proc/PaimonTablePartitionsProcDir.java
@@ -59,7 +59,9 @@ public class PaimonTablePartitionsProcDir extends PartitionsProcDir {
                 .getPartitions(table.getCatalogName(), table, listPartitionNames);
 
         for (PartitionInfo partitionInfo : tblPartitionInfo) {
-            partitionInfos.add(getPartitionInfo(partitionInfo));
+            if (partitionInfo != null) {
+                partitionInfos.add(getPartitionInfo(partitionInfo));
+            }
         }
         return partitionInfos;
     }
