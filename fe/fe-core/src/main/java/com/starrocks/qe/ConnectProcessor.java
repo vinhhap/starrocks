22f3fb165410181db24a1d607c538432eff88c69
wxl250059 <wxl250059@alibaba-inc.com>
2025-10-22 10:32:48 +0800

    [Stella][BP-BugFix] Fix the bug where UserProperty priority is lower than Session Variable (backport #63173) (#63217) Link: https://code.alibaba-inc.com/soe/stella/codereview/24037718 * [Stella][BP-BugFix] Fix the bug where UserProperty priority is lower than Session Variable (backport #63173) (#63217)
    Co-authored-by: Harbor Liu <460660596@qq.com>
    Signed-off-by: Drake Wang <wxl250059@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -39,6 +39,7 @@ import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.LiteralExpr;
 import com.starrocks.analysis.NullLiteral;
 import com.starrocks.authentication.AuthenticationMgr;
+import com.starrocks.authentication.UserProperty;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.Table;
@@ -798,11 +799,27 @@ public class ConnectProcessor {
             ctx.setRunningMultiTxnId(request.getMulti_txn_id());
         }
 
+        if (ctx.getCurrentUserIdentity() == null) {
+            TMasterOpResult result = new TMasterOpResult();
+            ctx.getState().setError(
+                    "Missing current user identity. You need to upgrade this Frontend to the same version as Leader Frontend.");
+            result.setMaxJournalId(GlobalStateMgr.getCurrentState().getMaxJournalId());
+            result.setPacket(getResultPacket());
+            return result;
+        }
+
         if (request.isSetUser_roles()) {
             List<Long> roleIds = request.getUser_roles().getRole_id_list();
             ctx.setCurrentRoleIds(new HashSet<>(roleIds));
         } else {
             ctx.setCurrentRoleIds(new HashSet<>());
+        }
+
+        UserIdentity userIdentity = ctx.getCurrentUserIdentity();
+        if (!userIdentity.isEphemeral()) {
+            UserProperty userProperty = GlobalStateMgr.getCurrentState().getAuthenticationMgr()
+                    .getUserProperty(ctx.getCurrentUserIdentity().getUser());
+            ctx.updateByUserProperty(userProperty);
         }
 
         // after https://github.com/StarRocks/starrocks/pull/43162, we support temporary tables.
@@ -874,18 +891,6 @@ public class ConnectProcessor {
         }
 
         ctx.setThreadLocalInfo();
-
-        if (ctx.getCurrentUserIdentity() == null) {
-            // if we upgrade Master FE first, the request from old FE does not set "current_user_ident".
-            // so ctx.getCurrentUserIdentity() will get null, and causing NullPointerException after using it.
-            // return error directly.
-            TMasterOpResult result = new TMasterOpResult();
-            ctx.getState().setError(
-                    "Missing current user identity. You need to upgrade this Frontend to the same version as Leader Frontend.");
-            result.setMaxJournalId(GlobalStateMgr.getCurrentState().getMaxJournalId());
-            result.setPacket(getResultPacket());
-            return result;
-        }
 
         StmtExecutor executor = null;
         try {
